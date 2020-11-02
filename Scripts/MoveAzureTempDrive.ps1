0351642c4e6b5985e41ab63842afba0695b97073
anjanidevio <35129036+anjanidevio@users.noreply.github.com>
2020-11-02 19:25:17 +0530

    Update MoveAzureTempDrive.ps1

--- Scripts/MoveAzureTempDrive.ps1
+++ Scripts/MoveAzureTempDrive.ps1
@@ -1,11 +1,11 @@
-﻿configuration MoveAzureTempDrive
+configuration MoveAzureTempDrive
 {
     param(
 		[Parameter(Mandatory)] 
         [string]$TempDriveLetter
     )
 
-    Import-DscResource -ModuleName MoveAzureTempDrive
+    Import-DscResource -ModuleName MoveAzureTempDrive
 
     Node localhost 
     {
@@ -51,21 +51,9 @@
 	   MoveAzureTempDrive MoveAzureTempDrive
        {
 		  
-		   TempDriveLetter = $TempDriveLetter  
-		   
+		   TempDriveLetter = $TempDriveLetter           
        }
-                   SetScript  = {
-                	Get-Partition -DriveLetter "D" | Set-Partition -NewDriveLetter "T"
-                	$TempDriveLetter = "T"
-			$TempDriveLetter = $TempDriveLetter + ":"
-			$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
-			#re-enable page file on new Drive
-			$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
-			Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{ Name = "$TempDriveLetter\pagefile.sys"; MaximumSize = 0; }
-			Restart-Computer -Force
-            }
       
 	}
 }
-
 
