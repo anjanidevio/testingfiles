c99acaff9c1c10eab6cb541a0549242cbfe079be
anjanidevio <35129036+anjanidevio@users.noreply.github.com>
2020-11-02 17:07:59 +0530

    Update MoveAzureTempDrive.ps1

--- Scripts/MoveAzureTempDrive.ps1
+++ Scripts/MoveAzureTempDrive.ps1
@@ -51,8 +51,19 @@
 	   MoveAzureTempDrive MoveAzureTempDrive
        {
 		  
-		   TempDriveLetter = $TempDriveLetter           
+		   TempDriveLetter = $TempDriveLetter  
+		   
        }
+                   SetScript  = {
+                	Get-Partition -DriveLetter "D" | Set-Partition -NewDriveLetter "T"
+                	$TempDriveLetter = "T"
+			$TempDriveLetter = $TempDriveLetter + ":"
+			$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
+			#re-enable page file on new Drive
+			$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
+			Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{ Name = "$TempDriveLetter\pagefile.sys"; MaximumSize = 0; }
+			Restart-Computer -Force
+            }
       
 	}
 }
